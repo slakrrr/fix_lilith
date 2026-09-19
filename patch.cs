52906cbe37fc0f143d0e8cd00938db6293e4ed11
slakr <15795849+slakr@user.noreply.gitee.com>
2026-09-19 12:34:54 +0800

    update plugin identifier, and more detailed comment

--- patch.cs
+++ patch.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 using HarmonyLib;
 
 
-[BepInPlugin("com.slakr.fixlilith", "FixLilith", "1.1.0")]
+[BepInPlugin("slakr.fixlilith", "FixLilith", "1.1.0")]
 public class FixLilithPlugin : BasePlugin {
     public override void Load() {
-        var harmony = new Harmony("com.slakr.fixlilith");
+        var harmony = new Harmony("slakr.fixlilith");
         harmony.PatchAll(typeof(SetLayeredWindowAttributesPatch));
         harmony.PatchAll(typeof(VirtualScreenBoundsPatch));
         // harmony.PatchAll(typeof(PrefsPatch));
@@ -21,7 +21,10 @@ public class FixLilithPlugin : BasePlugin {
 }
 
 // make virtual screen slightly smaller
-// to avoid gnome recognize it as fullscreen app
+// to avoid GNOME/Mutter (and some other Wayland compositors?)
+// recognizing it as a fullscreen app
+// so it can coexist with other fullscreen windows
+// and doesnt make GNOME hide its topbar
 [HarmonyPatch]
 static class VirtualScreenBoundsPatch {
     static MethodBase TargetMethod() =>
@@ -33,10 +36,10 @@ static class VirtualScreenBoundsPatch {
     }
 }
 
-// somehow game would restart after we change
-// virtual screen bound, which would cause
-// bepinex injection fails
-// so we simply 'disable' retart
+// somehow the game would restart after we change virtual screen bounds,
+// which would cause BepInEx injection failure
+// so we simply 'disable' restart
+// a bit crude but seemingly works fine
 [HarmonyPatch]
 static class NeedsBootstrapPatch {
     static MethodBase TargetMethod() =>
@@ -52,22 +55,25 @@ public class NoRestartPatch {
     static bool Prefix(string reason) { return false; }
 }
 
-// // seemingly useless, but i would keep this just for case
+// seemingly useless, but i will keep this just in case
+// // let the game write a smaller virtualHeight into registry
 // [HarmonyPatch]
 // static class PrefsPatch {
 //     static MethodBase TargetMethod() =>
 //         AccessTools.Method(AccessTools.TypeByName("WindowsVirtualScreenBootstrapper"),
 //                            "WriteScreenmanagerPrefs");
-
+//
 //     static void Prefix(ref int virtualWidth, ref int virtualHeight) {
 //         virtualHeight -= 12;
 //     }
 // }
 
 
-// game would call SetLayeredWindowAttributes(...) multiple times
-// with same parameters, which will cause flickering
-// so we simply intercept the same call
+// the game would call SetLayeredWindowAttributes(...) multiple times
+// with the same parameters, which causes flickering
+// (because thats essentially destroying and recreating surface repeatedly)
+// have no idea why the game do that
+// here we simply skip duplicated calls, seemingly works fine
 [HarmonyPatch]
 public class SetLayeredWindowAttributesPatch {
     private static bool   m_inited;
